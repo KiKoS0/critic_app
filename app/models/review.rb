973387786e6ddf747a4377d3a0c6fa50b61c9535
KiKoS <riadh.kikos123@hotmail.fr>
2018-02-28 19:37:18 +0100

    fix typo

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :prof
 
-  default_score -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :desc) }
   validates :prof_id, presence: true
   validates :content, presence: true, length: { maximum:200}
 
