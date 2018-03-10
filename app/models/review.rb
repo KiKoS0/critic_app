3acadfe784901566813b1c6337dd7da5191bfe15
KiKoS <riadh.kikos123@hotmail.fr>
2018-03-10 21:23:36 +0100

    Enhance visuals, Add better error messages and review opinion inclusion validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -11,5 +11,15 @@ class Review < ApplicationRecord
   validates :prof_id, presence: true
   validates :content, presence: true, length: { maximum:350000}
 
+  validates_inclusion_of :like, in: [true, false], message: "must be present"
+
+  HUMANIZED_ATTRIBUTES = {
+    :like => "An opinion",
+    :content => "Review content"
+  }
+
+  def self.human_attribute_name(attr, options={})
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
 
 end
